e1ca6e60c794513be1888b56319eea7d12c78fce
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-03-18 19:14:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 # copy the append.sh to user $HOME
 userPath=`echo /home/$USER`
-
 cp append.sh $userPath
 
-# Would like to get keybindings auto configured but DE dependent 
+# Let user run the driver script
+sudo chmod u+x append.sh
+
+# Done 
 echo "->Done! To run type: ./append.sh in ${userPath}"
 printf "\n-> [Note] Recommended to setup as a keyboard shortcut or alias :)	 " 
 printf "\n-> ! [Keep p-append-file.py in ${userPath}/build/p-append-file ]"
