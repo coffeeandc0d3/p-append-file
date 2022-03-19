4b2e3f7390638a8b94aa9be4a7cb0bbce4b4ba22
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-03-18 20:26:12 -0500

    Update p-append-file.py

--- p-append-file.py
+++ p-append-file.py
@@ -15,7 +15,7 @@ def insert(originalfile,string):
 # User input
 def main():
 	pastedText = input("Paste text to be appended to file: \n")
-	fileToAppend = input("File to be appended: (use ~/ or /home/<username>/ ) \n")
+	fileToAppend = input("File to be appended: (for $home type: /home/<username>/ ) \n")
 
 	insert(fileToAppend, pastedText)
 
