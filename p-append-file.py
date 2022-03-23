9fde078ab73e9e332c28cfd708e54c42457fe260
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-03-22 23:25:15 -0500

    fully working clipboard & patch-checking

--- p-append-file.py
+++ p-append-file.py
@@ -1,12 +1,14 @@
 import os
 import time 
+import prompt_toolkit
 
 # Writes new & old text into buffer, renames buffer to original filename
-def insert(originalfile,string):
+def insert(originalfile,strings):
 	with open(originalfile,'r') as f:
 		with open('buffer.txt','w') as f2:
-			f2.write(string + "\n\n")
-			f2.write(f.read())
+			for line in strings:
+				f2.write(line + "\n\n")
+				f2.write(f.read())
 		os.rename('buffer.txt',originalfile)
 	
 		print("Done!\nFile: " + originalfile + " appended. ")
@@ -14,10 +16,35 @@ def insert(originalfile,string):
 
 # User input
 def main():
-	pastedText = input("Paste text to be appended to file: \n")
-	fileToAppend = input("File to be appended: (for $home type: /home/<username>/<file> ) \n")
 
-	insert(fileToAppend, pastedText)
+	print('Enter or paste text to append: ')
+	
+	x =  input() 
+	rawInput = []
+
+#   Allows pasting multi-line text	
+	while x != '':  
+		rawInput.append(x) 
+		x = input()
+	print(rawInput)
+	
+	count = 0
+	while True:
+		
+		count += 1
+		print (count)
+		
+		if (count >= 2):
+			print("Sorry couldn't find that filepath. ")
+		
+		fileToAppend = input("File to be appended: (for $home type: /home/<username>/<file> ) \n")
+		
+		if(os.path.isfile(fileToAppend) == True):
+			count += 1
+			print(count)
+			break
+
+	insert(fileToAppend, rawInput)
 
 if __name__ == "__main__":
 	main()
