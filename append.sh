fd074d4e8ef4562aba588ac9cc43aa8e80a47aed
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-04-08 20:49:33 -0400

    support multiline paste

--- append.sh
+++ append.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 cd ~/build/p-append-file/
-python3 p-append-file.py
+
+rm buffer
+touch buffer
+
+userPath=`echo /home/$USER/build/p-append-file/buffer`
+echo "Append text: (ctrl + d when done)" && cat > $userPath
+
+# Press Ctrl+D to stop recording stuff into file
+cat $userPath
+
+python3 p-append-file.py $userPath
+
 
